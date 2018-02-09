e1b2ecda18e257be150bf1e8edf060a0ea562ed6
sky_k <sky_karlos@hotmail.com>
2018-02-09 04:25:17 -0500

    efecto glitch, limpiar intervalos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import * as p5 from 'p5';
+declare let p5: any;
 
 
 
@@ -77,6 +77,7 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   private destroyCanvas () {
     console.log('destroying canvas');
     this.p5.noCanvas();
+    this.p5 = null;
   }
 
   private sketch = (p: any) => {
@@ -93,6 +94,14 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     p.instanceAboutNode = null;
     p.instanceSkillsNode = null;
     p.instanceProyectsNode = null;
+    p.glitchImage = null;
+    p.glitchEffectObject = null;
+    p.drone = null;
+
+
+    p.loadImage('https://vignette.wikia.nocookie.net/gearsofwar/images/d/d0/Drone-l.jpg', function(img) {
+      p.drone = img ;
+    });
 
 
     p.setup = () => {
@@ -100,6 +109,7 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
      // p.frameRate(25);
      // p.drawingContext.shadowColor = 'rgba(220,255,220,0.8)';
      // p.drawingContext.shadowBlur = 4;
+
       p.rectMode(p.CENTER);
       p.imageMode(p.CENTER);
       p.textAlign(p.CENTER, p.CENTER);
@@ -164,6 +174,10 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
       }
 
    //   console.log(p.frameRate());
+
+      if (p.glitchImage != null) {
+        p.glitchEffectObject.show();
+      }
     };
 
     p.startLoading = () => {
@@ -424,6 +438,282 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
 
     };
 
+    p.glitch = function (img) {
+
+      this.channelLen = 4;
+      this.imgOrigin = img;
+      this.imgOrigin.loadPixels();
+      this.copyData = [];
+      this.flowLineImgs = [];
+      this.shiftLineImgs = [];
+      this.shiftRGBs = [];
+      this.scatImgs = [];
+      this.throughFlag = true;
+      this.countTFlag = 0;
+      this.copyData = new Uint8ClampedArray(this.imgOrigin.pixels);
+
+      // flow line
+      for (let i = 0; i < 1; i++) {
+        const o = {
+          pixels: null,
+          t1: p.floor(p.random(0, 1000)),
+          speed: p.floor(p.random(4, 24)),
+          randX: p.floor(p.random(24, 80))
+        };
+        this.flowLineImgs.push(o);
+      }
+
+      // shift line
+      for (let i = 0; i < 6; i++) {
+        const o = null;
+        this.shiftLineImgs.push(o);
+      }
+
+      // shift RGB
+      for (let i = 0; i < 1; i++) {
+        const o = null;
+        this.shiftRGBs.push(o);
+      }
+
+      // scat imgs
+      for (let i = 0; i < 3; i++) {
+        const scatImg = {
+          img: null,
+          x: 0,
+          y: 0
+        };
+        this.scatImgs.push(scatImg);
+      }
+
+
+      this.replaceData = function (destImg, srcPixels) {
+        for (let y = 0; y < destImg.height; y++) {
+          for (let x = 0; x < destImg.width; x++) {
+            let r, g, b, a;
+            let index;
+            index = (y * destImg.width + x) * this.channelLen;
+            r = index;
+            g = index + 1;
+            b = index + 2;
+            a = index + 3;
+            destImg.pixels[r] = srcPixels[r];
+            destImg.pixels[g] = srcPixels[g];
+            destImg.pixels[b] = srcPixels[b];
+            destImg.pixels[a] = srcPixels[a];
+          }
+        }
+        destImg.updatePixels();
+      };
+
+      this.flowLine = function (srcImg, obj) {
+
+        let destPixels,
+          tempY;
+        destPixels = new Uint8ClampedArray(srcImg.pixels);
+        obj.t1 %= srcImg.height;
+        obj.t1 += obj.speed;
+        // tempY = floor(noise(obj.t1) * srcImg.height);
+        tempY = p.floor(obj.t1);
+        for (let y = 0; y < srcImg.height; y++) {
+          if (tempY === y) {
+            for (let x = 0; x < srcImg.width; x++) {
+              let r, g, b, a;
+              let index;
+              index = (y * srcImg.width + x) * this.channelLen;
+              r = index;
+              g = index + 1;
+              b = index + 2;
+              a = index + 3;
+              destPixels[r] = srcImg.pixels[r] + obj.randX;
+              destPixels[g] = srcImg.pixels[g] + obj.randX;
+              destPixels[b] = srcImg.pixels[b] + obj.randX;
+              destPixels[a] = srcImg.pixels[a];
+            }
+          }
+        }
+        return destPixels;
+      };
+
+      this.shiftLine = function (srcImg) {
+
+        let offsetX;
+        let rangeMin, rangeMax;
+        let destPixels;
+        let rangeH;
+
+        destPixels = new Uint8ClampedArray(srcImg.pixels);
+        rangeH = srcImg.height;
+        rangeMin = p.floor(p.random(0, rangeH));
+        rangeMax = rangeMin + p.floor(p.random(1, rangeH - rangeMin));
+        offsetX = this.channelLen * p.floor(p.random(-40, 40));
+
+        for (let y = 0; y < srcImg.height; y++) {
+          if (y > rangeMin && y < rangeMax) {
+            for (let x = 0; x < srcImg.width; x++) {
+              let r, g, b, a;
+              let r2, g2, b2;
+              const a2 = null;
+              let index;
+
+              index = (y * srcImg.width + x) * this.channelLen;
+              r = index;
+              g = index + 1;
+              b = index + 2;
+              a = index + 3;
+              r2 = r + offsetX;
+              g2 = g + offsetX;
+              b2 = b + offsetX;
+              destPixels[r] = srcImg.pixels[r2];
+              destPixels[g] = srcImg.pixels[g2];
+              destPixels[b] = srcImg.pixels[b2];
+              destPixels[a] = srcImg.pixels[a];
+            }
+          }
+        }
+        return destPixels;
+      };
+
+      this.shiftRGB = function (srcImg) {
+
+        let randR, randG, randB;
+        let destPixels;
+        let range;
+
+        range = 16;
+        destPixels = new Uint8ClampedArray(srcImg.pixels);
+        randR = (p.floor(p.random(-range, range)) * srcImg.width + p.floor(p.random(-range, range))) * this.channelLen;
+        randG = (p.floor(p.random(-range, range)) * srcImg.width + p.floor(p.random(-range, range))) * this.channelLen;
+        randB = (p.floor(p.random(-range, range)) * srcImg.width + p.floor(p.random(-range, range))) * this.channelLen;
+
+        for (let y = 0; y < srcImg.height; y++) {
+          for (let x = 0; x < srcImg.width; x++) {
+            let r, g, b, a;
+            let r2, g2, b2;
+            const a2 = null;
+            let index;
+
+            index = (y * srcImg.width + x) * this.channelLen;
+            r = index;
+            g = index + 1;
+            b = index + 2;
+            a = index + 3;
+            r2 = (r + randR) % srcImg.pixels.length;
+            g2 = (g + randG) % srcImg.pixels.length;
+            b2 = (b + randB) % srcImg.pixels.length;
+            destPixels[r] = srcImg.pixels[r2];
+            destPixels[g] = srcImg.pixels[g2];
+            destPixels[b] = srcImg.pixels[b2];
+            destPixels[a] = srcImg.pixels[a];
+          }
+        }
+
+        return destPixels;
+      };
+
+      this.getRandomRectImg = function (srcImg) {
+        let startX;
+        let startY;
+        let rectW;
+        let rectH;
+        let destImg;
+        startX = p.floor(p.random(0, srcImg.width - 30));
+        startY = p.floor(p.random(0, srcImg.height - 50));
+        rectW = p.floor(p.random(30, srcImg.width - startX));
+        rectH = p.floor(p.random(1, 50));
+        destImg = srcImg.get(startX, startY, rectW, rectH);
+        destImg.loadPixels();
+        return destImg;
+      };
+
+      this.show = function () {
+
+        // restore the original state
+        this.replaceData(this.imgOrigin, this.copyData);
+
+        // sometimes pass without effect processing
+        const n = p.floor(p.random(100));
+        if (n > 75 && this.throughFlag) {
+          this.countTFlag++;
+          this.throughFlag = false;
+          setTimeout(() => {
+            this.throughFlag = true;
+          }, p.floor(p.random(40, 400)));
+        }
+        if (!this.throughFlag) {
+          p.push();
+          p.translate((p.width) / 2, (p.height) / 2);
+          if (this.countTFlag >= 7) {
+            console.log('3ntro');
+            p.image(this.imgOrigin, 0, 0);
+          }
+          p.pop();
+          return;
+        }
+
+        // flow line
+        this.flowLineImgs.forEach((v, i, arr) => {
+          arr[i].pixels = this.flowLine(this.imgOrigin, v);
+          if (arr[i].pixels) {
+            this.replaceData(this.imgOrigin, arr[i].pixels);
+          }
+        });
+
+        // shift line
+        this.shiftLineImgs.forEach((v, i, arr) => {
+          if (p.floor(p.random(100)) > 50) {
+            arr[i] = this.shiftLine(this.imgOrigin);
+            this.replaceData(this.imgOrigin, arr[i]);
+          } else {
+            if (arr[i]) {
+              this.replaceData(this.imgOrigin, arr[i]);
+            }
+          }
+        });
+
+        // shift rgb
+        this.shiftRGBs.forEach((v, i, arr) => {
+          if (p.floor(p.random(100)) > 65) {
+            arr[i] = this.shiftRGB(this.imgOrigin);
+            this.replaceData(this.imgOrigin, arr[i]);
+          }
+        });
+
+        p.push();
+        p.translate((p.width) / 2, (p.height) / 2);
+        p.image(this.imgOrigin, 0, 0);
+        p.pop();
+
+        // scat image
+        this.scatImgs.forEach((obj) => {
+          p.push();
+          p.translate((p.width) / 2, (p.height) / 2);
+          if (p.floor(p.random(100)) > 80) {
+            obj.x = p.floor(p.random(-this.imgOrigin.width * 0.3, this.imgOrigin.width * 0.7));
+            obj.y = p.floor(p.random(-this.imgOrigin.height * 0.1, this.imgOrigin.height));
+            obj.img = this.getRandomRectImg(this.imgOrigin);
+          }
+          if (obj.img) {
+            p.image(obj.img, obj.x, obj.y);
+          }
+          p.pop();
+        });
+
+      };
+
+    };
+
+    p.glishear = function () {
+
+        p.saveFrames('out', 'jpg', 1, 1, function (data) {
+          p.loadImage(data[0].imageData, function(img) {
+            p.glitchImage  = img ;
+            p.glitchEffectObject = new p.glitch(p.glitchImage);
+          });
+
+        });
+
+    };
+
     p.mouseClicked = () => {
         for (let i = p.nodes.length - 1; i >= 0; i--) {
 
@@ -446,6 +736,8 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
         }
       }
     };
+
+
 
   }
 
@@ -519,9 +811,10 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
             this.showCursorFour = true;
             this.showCursorThree = false;
             this.writing = false;
+            this.p5.glishear();
             setTimeout(() => {
               this.router.navigate([route]);
-            }, 2000);
+            }, 6000);
           }, 200);
 
         }
