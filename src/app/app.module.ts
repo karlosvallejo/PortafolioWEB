b8e51b64a4fad67675a963fa62f2de5b5ac6fff9
sky_k <sky_karlos@hotmail.com>
2018-07-17 04:53:19 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import {AngularFittextModule} from 'angular-fittext';
 
 import { AppComponent } from './app.component';
-import { BackgroundComponent } from './background/background.component';
-import { MenuComponent } from './menu/menu.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { SkillsComponent } from './skills/skills.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { RootsComponent } from './roots/roots.component';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'about', component: AboutComponent, pathMatch: 'full' },
-  { path: 'skills', component: SkillsComponent, pathMatch: 'full' },
-  { path: 'projects', component: ProjectsComponent, pathMatch: 'full' },
-  { path: '**' , redirectTo: ''}
-];
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BackgroundComponent,
-    MenuComponent,
-    HomeComponent,
-    AboutComponent,
-    SkillsComponent,
-    ProjectsComponent,
-    RootsComponent
+    AppComponent
   ],
   imports: [
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: false } // <-- debugging purposes only
-    ),
-    BrowserModule,
-    BrowserAnimationsModule,
-    AngularFittextModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
