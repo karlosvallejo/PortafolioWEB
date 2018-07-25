24ec74633aaa63fdf1c43bf0722bd23e96bc77b2
sky_k <sky_karlos@hotmail.com>
2018-07-24 23:15:37 -0500

    first loading page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {
   Router,
   Event as RouterEvent,
@@ -13,33 +13,36 @@ import {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title: string;
   loading: boolean;
 
   constructor(private router: Router) {
-
-    this.loading = false;
+    this.loading = true;
     this.title = 'Hominid Interactive';
+  }
 
-     this.router.events.subscribe((event: RouterEvent) => {
-        switch (true) {
-          case event instanceof NavigationStart: {
-            this.loading = true;
+  ngAfterViewInit(): void {
+    this.router.events.subscribe((event: RouterEvent) => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = true;
 
-            break;
-          }
-
-          case event instanceof NavigationEnd:
-          case event instanceof NavigationCancel:
-          case event instanceof  NavigationError: {
-            this.loading = false;
-            break;
-          }
-          default: {
-            break;
-          }
+          break;
         }
-     });
+
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof  NavigationError: {
+         // setTimeout(() => { // here
+            this.loading = false;
+         // }, 5000);
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {APP_INITIALIZER, NgModule} from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {AngularFittextModule} from 'angular-fittext';
 
+import {DelayResolver} from './delay.resolver';
 import { AppComponent } from './app.component';
 import { BackgroundComponent } from './background/background.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { RootsComponent } from './roots/roots.component';
@@ -17,7 +17,7 @@ import { ThreeComponentComponent } from './three-component/three-component.compo
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'about', component: AboutComponent, pathMatch: 'full' },
+  { path: 'about', loadChildren: './test/test.module#TestModule', pathMatch: 'full', resolve: {dicho : DelayResolver} },
   { path: 'skills', component: SkillsComponent, pathMatch: 'full' },
   { path: 'projects', component: ProjectsComponent, pathMatch: 'full' },
   { path: '**' , redirectTo: ''}
@@ -31,7 +31,6 @@ const appRoutes: Routes = [
     BackgroundComponent,
     MenuComponent,
     HomeComponent,
-    AboutComponent,
     SkillsComponent,
     ProjectsComponent,
     RootsComponent,
@@ -58,7 +57,7 @@ const appRoutes: Routes = [
       };
     },
     multi: true
-  }],
+  }, DelayResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
