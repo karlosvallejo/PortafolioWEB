f345ef81ae5a7737538d156b46d5b0efad9a5eac
sky_k <sky_karlos@hotmail.com>
2018-09-03 13:50:49 -0500

    styles

--- src/app/otherComponents/projects/projects.component.ts
+++ src/app/otherComponents/projects/projects.component.ts
@@ -11,7 +11,10 @@ export class ProjectsComponent implements OnInit {
 
 
   constructor() {
-    this.projectsArray = [new ProjectListNode('EcoInteractive', 'un proyecto interactvo de papus para papus',
+    this.projectsArray = [new ProjectListNode('EcoInteractive', 'Proyecto realizado en el curso Ambientes Inteligentes con ' +
+      'el objetivo de fomentar conocimiento sobre las labores medioambientales de smurfit kappa. Para el prototipo de creo ' +
+      'una version del famoso juego "El Sapo", donde la informacion era mostrada a partir de las interacciones del usuario, se utilizo ' +
+      'un arduino, una raspberry pi, diodos infrarrojos y una pantalla externa.',
       'https://eco-interactive.firebaseapp.com/', 'PROGRAMMING'), new ProjectListNode('Hoy es Diseño',
       'Pues la web de hed bro', 'hed.com', 'WEB'), new ProjectListNode('EcoInteractive', 'un proyecto interactvo de papus para papus',
       'https://eco-interactive.firebaseapp.com/', 'PROGRAMMING'),  new ProjectListNode('Hoy es Diseño',
